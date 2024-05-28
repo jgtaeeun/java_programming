ce21b810cd9b556aa5919af060c00005b05a6955
jgtaeeun <167741370+jgtaeeun@users.noreply.github.com>
2024-05-28 22:12:08 +0900

    Update train_ch2_6.java

--- Hproject/src/Chap2_기본자료구조/train_ch2_6.java
+++ Hproject/src/Chap2_기본자료구조/train_ch2_6.java
@@ -7,24 +7,86 @@ public class train_ch2_6 {
 	public train_ch2_6() {
 		// TODO Auto-generated constructor stub
 	}
-	static void inputData(double [int row][int col] data) {//double 난수 0.0 ~ 1.0 생성
-		Random rand=new Random();
-		for (int r = 0 ;  r<row ; r++){
-		   for (int c = 0 ;  c<col ; c++){
-			data[r][c]=rand.nextDouble()
-		   }
+	static void inputData(double[][] data) {//double 난수 0.0 ~ 1.0 생성
+		Random rand =new Random();
+		for (int r =0 ; r <data.length ;r ++) {
+			for (int c =0 ; c <data[0].length ;c ++) {
+				data[r][c]=rand.nextDouble();
+			}
+		}
+
+	}
+	static void showData(String txt,double[][] data) {//주어진 문자열을 출력하고 배열을 2차원 형태로 출력
+		System.out.println(txt);
+		for (int r =0 ; r <data.length ;r ++) {
+			for (int c =0 ; c <data[0].length ;c ++) {
+				System.out.print(data[r][c]);
+				System.out.print(" ");
+			}
+		     System.out.println();
 		}
 	}
-	static void showData(string txt, double [int row][int col] data ) {//주어진 문자열을 출력하고 배열을 2차원 형태로 출력
-		System.out.print(txt);
+	
+	
+
+
+	//}
+	static double [][] addMatrix(double [][] data1,double [][] data2) {//행렬 덧셈 결과를 리턴
 		
-		for (int r = 0 ;  r<row ; r++){
-		   for (int c = 0 ;  c<col ; c++){
-			System.out.print(data[r][c])
-		   }
-		   System.out.println()
+		double[][] add_data=new double [data1.length][data1[0].length];
+		for (int r =0 ; r <data1.length ;r ++) {
+			for (int c =0 ; c <data1[0].length ;c ++) {
+				add_data[r][c]=data1[r][c]+data2[r][c];
+			}
+		}
+		return add_data;
+		
+	}
+	static double [][] multiplyMatrix(double [][] data1,double [][] data2) {//행렬 곱셈 결과를 리턴
+		double[][] multi_data=new double [data1.length][data2[0].length];
+		
+		for (int r =0 ; r <data1.length ;r ++) {
+			for(int k=0 ; k<data2[0].length;k++) {
+				for (int c =0 ; c <data2.length ;c ++) {
+				multi_data[r][k]=data1[r][c]+data2[c][k];
+				}
+			}
+		}
+		
+		return multi_data;
+	}
+		
+	static double [][] transposeMatrix(double [][] data) {//전치 행렬을 리턴
+		double[][] trans_data=new double [data[0].length][data.length];
+		for (int c =0 ; c <data[0].length ;c ++) {
+			for (int r =0 ; r <data.length ;r ++) {
+						trans_data[c][r]=data[r][c];
+			}
+		}
+		return trans_data;
 	}
 
+	static boolean equals(double [][] data1,double [][] data2) {//두 행렬의 사이즈가 같고 값도 모두 같아야 행렬 2개는 equals을 true로 리턴
+		boolean b=true;
+		
+		if (data1.length !=data2.length) {
+			b=false;
+		}
+		else {	
+			for (int r =0 ; r <data1.length ;r ++) {
+					for (int c =0 ; c <data1[0].length ;c ++) {
+						if (data1[r][c]!=data2[r][c]) {
+							b=false;
+							break;
+						}
+					}
+			}
+		}
+		
+	return b;
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		double [][]A = new double[2][3];
