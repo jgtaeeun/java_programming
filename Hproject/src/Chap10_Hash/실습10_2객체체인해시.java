beb5a7db936a717fc28c1e114fcaca30ca3aa7c6
jgtaeeun <jas49258667@gmail.com>
2024-07-02 13:20:52 +0900

    0702객체체인해시

--- Hproject/src/Chap10_Hash/실습10_2객체체인해시.java
+++ Hproject/src/Chap10_Hash/실습10_2객체체인해시.java
@@ -16,6 +16,8 @@ class SimpleObject5 {
 	String no; // 회원번호
 	String name; // 이름
 
+	
+	
 	void scanData(String guide, int sw) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println(guide + "할 데이터를 입력하세요." + sw);
@@ -56,7 +58,14 @@ class ChainHash5 {
 		private Node2 next; // 뒤쪽 포인터(뒤쪽 노드에 대한 참조)
 		// --- 생성자(constructor) ---//
 
+		public Node2(SimpleObject5 data, Node2 next) {
+			this.data=data;
+			this.next=next;
+		}
 		
+		SimpleObject5 getData() {
+			return data;
+		}
 	}
 
 	private int size; // 해시 테이블의 크기
@@ -65,25 +74,74 @@ class ChainHash5 {
 //--- 생성자(constructor) ---//
 	public ChainHash5(int capacity) {
 		size=capacity;
+		table=new Node2[capacity];
 	}
-
+	
+	public int hashValue(int key) {
+		return (key*key*31)%size;
+	}
 //--- 키값이 key인 요소를 검색(데이터를 반환) ---//
 	public int search(SimpleObject5 st, Comparator<? super SimpleObject5> c) {
+		int hash = hashValue(Integer.parseInt(st.no));
+		Node2 p = table[hash];
 		
+		while(p!=null) {
+			if(c.compare(p.data,st)==0) {
+				return Integer.parseInt(st.no);
+			}
+			p=p.next;
+			
+				
+		}
+		return -1;
 	}
 
 //--- 키값이 key인 데이터를 data의 요소로 추가 ---//
 	public int add(SimpleObject5 st, Comparator<? super SimpleObject5> c) {
+		int hash = hashValue(Integer.parseInt(st.no));
+		Node2 p = table[hash];
 		
+		while(p!= null) {
+			if(c.compare(p.data,st)==0) {
+				return 0; //같은 데이터가 들어있다.
+			}
+			p=p.next;
+		}
+		Node2 tmp =  new Node2( st, table[hash]);
+		table[hash]=tmp;
+		return Integer.parseInt(st.no);
 	}
 
 //--- 키값이 key인 요소를 삭제 ---//
 	public int delete(SimpleObject5 st, Comparator<? super SimpleObject5> c) {
-		
+		int hash = hashValue(Integer.parseInt(st.no));
+		Node2 p = table[hash];
+		Node2 pp = null;
+		while(p!=null) {
+			if(c.compare(p.data,st)==0) {
+				if(pp==null)
+					table[hash]=p.next;
+				else
+					pp.next=p.next;
+				return Integer.parseInt(st.no);
+			}
+			pp=p;
+			p=p.next;
+		}
+		return -1;
 	}
 
 //--- 해시 테이블을 덤프(dump) ---//
 	public void dump() {
+		for(int i=0 ; i<size; i++) {
+			Node2 p = table[i];
+			System.out.printf("%d :" , i);
+			while(p!=null) {
+				System.out.printf("->[%s, %s]" , p.data.no, p.data.name);
+				p=p.next;
+			}
+			System.out.println();
+		}
 		
 	}
 }
@@ -138,29 +196,29 @@ public class 실습10_2객체체인해시 {
 				data = new SimpleObject5();
 				data.scanData("삽입", SimpleObject5.NO | SimpleObject5.NAME);
 				result = hash.add(data, SimpleObject5.NO_ORDER);
-				if (result == 1)
+				if (result == 0)
 					System.out.println(" 중복 데이터가 존재한다");
 				else
-					System.out.println(" 입력 처리됨");
+					System.out.printf(" %d 입력 처리됨", result);
 				break;
 			case Delete:
 				// Delete
 				data = new SimpleObject5();
 				data.scanData("삭제", SimpleObject5.NO);
 				result = hash.delete(data, SimpleObject5.NO_ORDER);
-				if (result == 1)
-					System.out.println(" 삭제 처리");
-				else
+				if (result == -1)
 					System.out.println(" 삭제 데이터가 없음");
+				else
+					System.out.printf("%d 삭제 처리됨",result);
 				break;
 			case Search:
 				data = new SimpleObject5();
 				data.scanData("검색", SimpleObject5.NO);
 				result = hash.search(data, SimpleObject5.NO_ORDER);
-				if (result == 1)
-					System.out.println(" 검색 데이터가 존재한다");
-				else
+				if (result ==-1)
 					System.out.println(" 검색 데이터가 없음");
+				else
+					System.out.printf("%d  검색 데이터가 존재한다",result);
 				break;
 			case Show:
 				hash.dump();
